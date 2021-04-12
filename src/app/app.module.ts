3520935a1c61a11eb3b8f748c9e108dbf99a7446
Roland Strodthoff <roland@webjoy.de>
2021-04-12 20:11:46 +0200

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UserWorkingHoursComponent} from './Components/user-working-hours/user-working-hours.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {UserService} from "./Services/user.service";
+import {HttpClient} from '@angular/common/http';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserWorkingHoursComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule
   ],
-  providers: [],
+  exports: [MatTabsModule],
+  providers: [MatTabsModule, UserService, HttpClient, MatTableModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
